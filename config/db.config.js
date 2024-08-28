b68ebc14812717402fb2a2cfa90e5dd8a889a73f
Lina Muñoz <linamunoz438@gmail.com>
2024-08-27 21:12:56 -0500

    Conexión Base de Datos

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB conectado");
+    // Analizar la URI para extraer el nombre de la base de datos
+    const mongoURI = process.env.MONGO_URI;
+    const dbName = mongoURI.match(/\/([^\/?]+)(\?|$)/)[1];
+
+    await mongoose.connect(mongoURI);
+    console.log(`MongoDB conectado a la base de datos: ${dbName}`);
   } catch (error) {
     console.error("Error de conexión a MongoDB:", error);
     process.exit(1); // Terminar el proceso si no se puede conectar
