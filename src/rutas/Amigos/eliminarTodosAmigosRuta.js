075f16fdd2b60151d1f30e5ce34b5c01c80fadec
Lina Muñoz <linamunoz438@gmail.com>
2024-12-18 00:46:48 -0500

    Para eliminar todos los amigos

--- src/rutas/Amigos/eliminarTodosAmigosRuta.js
+++ src/rutas/Amigos/eliminarTodosAmigosRuta.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const eliminarTodosAmigos = require("../../controladores/Amigos/eliminarTodosAmigos");
-const verificarToken = require("../../middlewares/varificarToken.js"); // Middleware para verificar el token
 
-router.delete("/eliminar/todos/amigos/:userId", verificarToken, eliminarTodosAmigos);
+router.delete("/eliminar/todos/amigos/:userId", eliminarTodosAmigos);
 
 module.exports = router;
